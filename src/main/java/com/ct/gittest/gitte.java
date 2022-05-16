75a171ac579739190505d4254aba231857213e17
chen <359868274@qq.com>
2022-05-16 10:05:12 +0800

    third commit

--- src/main/java/com/ct/gittest/gitte.java
+++ src/main/java/com/ct/gittest/gitte.java
@@ -4,5 +4,6 @@ public class gitte {
     public static void main(String[] args) {
         System.out.println(123);
         System.out.println(11111);
+        System.out.println(1111111);
     }
 }
